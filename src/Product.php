91ce95fa378d2b53f078c20e8f5e14c9fd6356c0
Matthew Lowe <lowe.matthew@mail.com>
2023-12-05 05:11:18 +0000

    :sparkles: Add basic crawling functionality

--- src/Product.php
+++ src/Product.php
@@ -4,5 +4,12 @@ namespace App;
 
 class Product
 {
+    public string $name;
+    public string $price;
 
+    public function __construct($name, $price)
+    {
+        $this->name = $name;
+        $this->price = $price;
+    }
 }
--- src/Scrape.php
+++ src/Scrape.php
@@ -3,6 +3,9 @@
 namespace App;
 
 require 'vendor/autoload.php';
+require 'Product.php';
+
+use Symfony\Component\DomCrawler\Crawler;
 
 class Scrape
 {
@@ -12,7 +15,22 @@ class Scrape
     {
         $document = ScrapeHelper::fetchDocument('https://www.magpiehq.com/developer-challenge/smartphones');
 
-        file_put_contents('output.json', json_encode($this->products));
+        $this->products = $document->filter('.product')->each(function (Crawler $node, $i) : Product {
+            $name = $node->filter('h3')->text();
+
+            $price = $node->filter('div div')->reduce(function (Crawler $price_node, $j) : bool {
+                return str_contains($price_node->text(), '£');
+            })->last()->text();
+            
+            return new Product($name, $this->parse_price($price));
+        });
+        
+        file_put_contents('output.json', json_encode($this->products, JSON_PRETTY_PRINT));
+    }
+
+    private function parse_price($price_str)
+    {
+        return doubleval(str_replace("£", '', $price_str));
     }
 }
 
